095b79166584cfd0dbab12e612825151d31f2330
IlyaDi1305 <didorenko1305@gmail.com>
2024-10-10 00:32:53 +0200

    HW02 #2 correction of comments

--- src/main/java/ru/javawebinar/topjava/service/MealService.java
+++ src/main/java/ru/javawebinar/topjava/service/MealService.java
@@ -5,7 +5,6 @@ import ru.javawebinar.topjava.model.Meal;
 import ru.javawebinar.topjava.repository.MealRepository;
 
 import java.time.LocalDate;
-import java.time.LocalTime;
 import java.util.List;
 
 import static ru.javawebinar.topjava.util.ValidationUtil.checkNotFoundWithId;
@@ -39,7 +38,7 @@ public class MealService {
         return repository.save(meal, userId);
     }
 
-    public List<Meal> getAllFiltered(int userId, LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
+    public List<Meal> getAllFiltered(int userId, LocalDate startDate, LocalDate endDate) {
         return repository.getAllFiltered(userId, startDate, endDate);
     }
 }
--- src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealRestController.java
@@ -57,7 +57,7 @@ public class MealRestController {
 
     public List<MealTo> getAllFiltered(LocalDate startDate, LocalDate endDate, LocalTime startTime, LocalTime endTime) {
         log.info("getting all filtered meals");
-        List<Meal> filteredMeals = service.getAllFiltered(SecurityUtil.authUserId(), startDate, endDate, startTime, endTime);
+        List<Meal> filteredMeals = service.getAllFiltered(SecurityUtil.authUserId(), startDate, endDate);
         return MealsUtil.getFilteredTos(filteredMeals, SecurityUtil.authUserCaloriesPerDay(), startTime, endTime);
     }
 }
