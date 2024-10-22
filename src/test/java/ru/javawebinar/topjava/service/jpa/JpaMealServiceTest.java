4227b443a22ce5688777cbe91ab2638ea55afb2c
IlyaDi1305 <116590021+IlyaDi1305@users.noreply.github.com>
2024-10-22 10:26:33 +0200

    HW05 refactoring AbstractServiceTest

--- src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/JpaMealServiceTest.java
@@ -3,7 +3,9 @@ package ru.javawebinar.topjava.service.jpa;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.service.AbstractMealServiceTest;
 
-@ActiveProfiles("jpa")
+import static ru.javawebinar.topjava.Profiles.JPA;
+
+@ActiveProfiles(JPA)
 public class JpaMealServiceTest extends AbstractMealServiceTest {
 
 }
