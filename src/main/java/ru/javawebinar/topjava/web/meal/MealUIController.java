44fa1131d047efa7886740989b200460eb8b3565
IlyaDi1305 <116590021+IlyaDi1305@users.noreply.github.com>
2024-11-11 12:25:39 +0100

    HW08 init commit

---
+++ src/main/java/ru/javawebinar/topjava/web/meal/MealUIController.java
@@ -0,0 +1,10 @@
+package ru.javawebinar.topjava.web.meal;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/meals")
+public class MealUIController extends AbstractMealController{
+
+}
